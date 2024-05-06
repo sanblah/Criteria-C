a96b6c0d2a50193bd593931ee72ecaa4b9f613c5
sanblah <97375658+sanblah@users.noreply.github.com>
2024-05-06 11:37:39 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,25 @@
+function product(){
+    window.location = "Productpagebutterkhari.html"
+}
+function home(){
+    window.location = "index.html" 
+}
+function imgchange(){
+    var img = document.getElementById('image');
+    img.src = 'cookieangle1.png';
+}
+function imgchange1(){
+    var img = document.getElementById('image');
+    img.src = 'cookieangle2.png';
+}
+function imgchange2(){
+    var img = document.getElementById('image');
+    img.src = 'cookieangle3.png';
+}
+function imgchange3(){
+    var img = document.getElementById('image');
+    img.src = 'cookieangle4.png';
+}
+function produc(){
+    window.location = "about.html"
+}
